3804862f71b769e785085deeb754565ceeb1d13a
yiyikkk <1071080142@qq.com>
2022-02-16 22:26:26 -0500

    first commit

---
+++ code/backend/src/main/java/com/stylebox/StyleBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stylebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleBoxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StyleBoxBackendApplication.class, args);
+    }
+
+}
---
+++ code/backend/src/test/java/com/stylebox/StyleBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleBoxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
