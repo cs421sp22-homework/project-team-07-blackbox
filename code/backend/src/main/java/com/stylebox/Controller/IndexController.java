b6a778ee7537d10e155ebf6315ed6a16adae8362
alice <alice@alices-mbp.acasmart.jh.edu>
2022-02-17 22:49:47 -0500

    connected frontend and backend

--- code/backend/src/main/java/com/stylebox/Controller/IndexController.java
+++ code/backend/src/main/java/com/stylebox/Controller/IndexController.java
@@ -1,15 +1,18 @@
 package com.stylebox.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
 public class IndexController {
     @GetMapping("/index")
     public String index() {
-        return "HelloWorld";
+        return "Hello World";
     }
 }
