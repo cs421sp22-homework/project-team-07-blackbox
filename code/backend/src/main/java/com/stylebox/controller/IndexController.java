8af03570bdd9b2e1696a1f09743f9bf980b843c6
yachialice <4003041997@163.com>
2022-02-19 18:37:38 -0500

    modify url according to deployed url

--- code/backend/src/main/java/com/stylebox/controller/IndexController.java
+++ code/backend/src/main/java/com/stylebox/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="https://stylebox5.herokuapp.com/")
+@CrossOrigin(origins={"http://stylebox5.herokuapp.com","https://stylebox5.herokuapp.com"})
 @RestController
 public class IndexController {
     @GetMapping("/index")
