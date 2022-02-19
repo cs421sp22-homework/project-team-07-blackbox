98f3d2ef4d6493175097d6d1bccda897e41e84e5
Zhuoying Li <daritenlzy@gmail.com>
2022-02-19 12:39:26 -0500

    add package

--- code/backend/src/main/java/com/stylebox/controller/IndexController.java
+++ code/backend/src/main/java/com/stylebox/controller/IndexController.java
@@ -1,12 +1,8 @@
-package com.stylebox.Controller;
+package com.stylebox.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
