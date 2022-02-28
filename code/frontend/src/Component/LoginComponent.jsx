df89f27921fce3bef589ace59b0ab02a09e93adb
wulu <wulu0120@gmail.com>
2022-02-28 01:23:23 -0500

    front end login without token

--- code/frontend/src/Component/LoginComponent.jsx
+++ code/frontend/src/Component/LoginComponent.jsx
@@ -1,67 +1,94 @@
 import React, {Component} from 'react';
-import LoginService from '../api/styleBox/LoginService';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthenticationService from '../api/styleBox/AuthenticationService';
 
-class Login extends Component{
+class LoginCompnent extends Component{
     constructor(props){
         super(props)
         this.state={
+            // login 参数
             username: '',
             password: '',
             hasLoginFailed: false,
             showSuccessMessage: false
         }
+
+        // bind 方法
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+
     }
 
-    componentDidMount(){
-        LoginService.executeLoginService()
-            .then(response=>this.setState({info: response.data}))
-            .catch(error => console.log(error.response))
+    // 根据输入更改变量值
+    handleChange(event) {
+        this.setState(
+            {
+                [event.target.name]
+                    : event.target.value
+            }
+        )
     }
+
+    // submit btn function, 调用login method in AuthenticationService.js
+    loginClicked() {
+        console.log('Send username ' + this.state.username + 'password ' + this.state.password)
+        
+        AuthenticationService
+        .executeJwtAuthenticationService(this.state.username, this.state.password)
+        .then((response) => {
+            // 处理前端返回的token，待写
+            console.log(response.data.token)
+        })
+        .catch(() => {
+            this.setState({showSuccessMessage: false})
+            this.setState({hasLoginFailed: true})
+        })
+    }
+    
 
     render(){
         return(
             <div className='container'>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light my-3">
-                    <a class="navbar-brand" href="#">StyleBox</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <a className="navbar-brand" href="#">StyleBox</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Stylist </a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Stylist </a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Shopping</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Shopping</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Community</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Community</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Login</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Login</a>
                             </li>
                         </ul>
                         
                     </div>
                 </nav>
 
-                <div class="alert alert-primary w-50" role="alert">
+                <div className="alert alert-primary w-50" role="alert">
                 
                     <form> 
                         <h1 className='my-3'>Login <span class="badge badge-secondary"></span></h1>
                         <div className='row mt-5'>
                             <label htmlFor="inputAccount" className='col-sm-2 col-form-label'>Account: </label>
                             <div className='col-sm-10'>
-                                <input type="text" className='form-control' name='username' placeholder='Enter Email or UserName Here' />
+                                <input type="text" className='form-control' name='username' placeholder='Enter Email or UserName Here' value={this.state.username} onChange={this.handleChange}/>
                             </div>
                         </div>
 
                         <div className='row my-3'>
                             <label htmlFor="inputPassword" className='col-sm-2 col-form-label'>Password: </label>
                             <div className='col-sm-8'>
-                                <input type="text" className='form-control' name='password' placeholder='Enter Password Here' />
+                                <input type="text" className='form-control' name='password' placeholder='Enter Password Here' value={this.state.password} onChange={this.handleChange}/>
                             </div>
                             <div className='form-check col-sm-2'>
                                 <input type="checkbox" className='form-check-input' id = 'hiddenbox'/>
@@ -70,7 +97,7 @@ class Login extends Component{
                                 </label>
                             </div>
                         </div>
-                            <button type='submit' className='btn btn-primary my-3'>Login</button>
+                            <button type='submit' className='btn btn-primary my-3' onClick={this.loginClicked}>Login</button>
 
                     </form>
                 </div>
@@ -79,4 +106,4 @@ class Login extends Component{
     }
 }
 
-export default Login
+export default LoginCompnent
