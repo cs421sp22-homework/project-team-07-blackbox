d7d18c53099415c3a1398406d9b91005574a4332
yiyikkk <1071080142@qq.com>
2022-03-02 15:14:37 -0500

    #12 #13 try to connect with frontend

--- code/frontend/src/api/styleBox/AuthenticationService.js
+++ code/frontend/src/api/styleBox/AuthenticationService.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 import { API_URL } from '../../Constants'
+import data from "bootstrap/js/src/dom/data";
+axios.defaults.withCredentials = true;
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 
 class AuthenticationService {
-    
+
     // Login 方法传递username, password到后端
     executeJwtAuthenticationService(usernameValue, passwordValue){
-        let user = JSON.stringify({username: usernameValue, password: passwordValue})
-        return axios.post(`${API_URL}/login`, {
-            user
-        })
+        // let user = {username: usernameValue, password: passwordValue}
+        return axios.post(`${API_URL}/login`, {username: usernameValue, password: passwordValue},{withCredentials: true})
     }
 
     // 处理前端返回的token方法，代写
