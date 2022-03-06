81478855215561839d96201890da5d2029d716be
yachialice <4003041997@163.com>
2022-03-06 02:11:16 -0500

    change crossorigin & backend_url

--- code/frontend/src/Constants.js
+++ code/frontend/src/Constants.js
@@ -1,3 +1,4 @@
-export const API_URL = 'https://stylebox-server.herokuapp.com'   //线上路径
+// export const API_URL = 'https://stylebox-server.herokuapp.com'   //线上路径
+export const API_URL = 'https://stylebox-server.netlify.app'
 // export const API_URL = 'http://localhost:8080'    // 线下测试路径
 export const JPA_API_URL = 'http://localhost:8080/jpa'
