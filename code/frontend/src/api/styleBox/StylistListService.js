e9161398cc8ec7dcb4ef789509e97bef9e48b039
Zhuoying Li <daritenlzy@gmail.com>
2022-03-18 18:28:44 -0400

    NA

--- code/frontend/src/api/styleBox/StylistListService.js
+++ code/frontend/src/api/styleBox/StylistListService.js
@@ -7,15 +7,6 @@ axios.defaults.withCredentials=true
 class StylistListService {
     // Get order
     getStylistList(pageValue,styleValue,sortValue,searchValue,limitValue){
-<<<<<<< HEAD
-        let param = {page: pageValue, style:styleValue,sort: sortValue,search:searchValue,limit:limitValue}
-        return axios.get(`${API_URL}/stylists`,param)
-    }
-
-    searchInfo(info){
-        console.log(info)
-        return axios.get(`${API_URL}/stylists`,info)
-=======
         let params = {page: pageValue, style:styleValue,sort: sortValue,search:searchValue,limit:limitValue}
         console.log(params)
         return axios.get(`${API_URL}/stylists`,
@@ -25,7 +16,6 @@ class StylistListService {
     searchInfo(params){
         console.log(params)
         return axios.get(`${API_URL}/stylists`,params)
->>>>>>> cab571dd1eb3f352428bbfac7c15c9f9d0de4c79
     }
 
 
