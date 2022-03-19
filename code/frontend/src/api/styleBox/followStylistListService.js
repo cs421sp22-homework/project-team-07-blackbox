8095da9fe46da0c60af659c36e0d07fd27e14a8e
your_github_username <your_github_email>
2022-03-19 12:52:16 -0400

    followstylistservice

--- code/frontend/src/api/styleBox/followStylistListService.js
+++ code/frontend/src/api/styleBox/followStylistListService.js
@@ -8,13 +8,13 @@ class followStylistListService {
     getStylistList(pageValue,styleValue,sortValue,searchValue,limitValue){
         let params = {page: pageValue, style:styleValue,sort: sortValue,search:searchValue,limit:limitValue}
         console.log(params)
-        return axios.get(`${API_URL}/stylists`,
+        return axios.get(`${API_URL}/followStylist`,
             {params:{page: pageValue, style:styleValue,sort: sortValue,search:searchValue,limit:limitValue}})
     }
 
     searchInfo(params){
         console.log(params)
-        return axios.get(`${API_URL}/stylists`,params)
+        return axios.get(`${API_URL}/followStylist`,params)
     }
 
 
