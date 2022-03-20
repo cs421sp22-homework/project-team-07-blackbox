e644759a788850be3acfc8356113a47a3564d8bd
yiyikkk <1071080142@qq.com>
2022-03-19 22:51:58 -0400

    modify gender

--- code/backend/src/main/java/com/stylebox/service/UserService.java
+++ code/backend/src/main/java/com/stylebox/service/UserService.java
@@ -163,7 +163,7 @@ public class UserService {
     public void createCustomerProfile(User user, CustomerProfileDTO customerProfileDTO){
         Set<String> styleSet = customerProfileDTO.getStyleSet();
         modifyStyle(styleSet, user);
-        user.setFacebook("F");
+        user.setGender(customerProfileDTO.getGender());
         modelMapper.map(customerProfileDTO, user.getCustomerInformation());
         customerInformationRepository.save(user.getCustomerInformation());
         userRepository.save(user);
