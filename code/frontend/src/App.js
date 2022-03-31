f9e9cee74a592bda38fa2d9657ccbfbce9330762
wulu <wulu0120@gmail.com>
2022-03-31 00:20:38 -0400

    #52 payment feature UI design

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -20,6 +20,7 @@ import FollowStylist from"./styleBox/customer/followStylistList"
 import NavBarCustomer from "./styleBox/navBar_footer/NavBarCustomer";
 import NavBarStylist from "./styleBox/navBar_footer/NavBarStylist";
 import DropDown from "./styleBox/navBar_footer/Dropdown"
+import PayOrder from "./styleBox/order/PayOrder"
 
 
 export default function App() {
@@ -45,6 +46,7 @@ export default function App() {
               <AuthenticatedRoute path="/orderDetail" component={OrderDetail} />
               <AuthenticatedRoute path="/stylist/homepage" component={ReadStylist}/>
               <AuthenticatedRoute path="/order" component={CreateOrder}/>
+              <AuthenticatedRoute path="/payOrder" component={PayOrder}/>
           </Switch>
       </>
   </Router>
