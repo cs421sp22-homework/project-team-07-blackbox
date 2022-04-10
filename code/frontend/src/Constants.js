f59694fa94dd60e0a858127fad5928a2e97d162d
yiyikkk <1071080142@qq.com>
2022-04-09 23:21:01 -0400

    aliyun api

--- code/frontend/src/Constants.js
+++ code/frontend/src/Constants.js
@@ -1,6 +1,6 @@
 
 // export const API_URL = 'https://stylebox-server.herokuapp.com'   //线上路径
-export const API_URL = 'http://121.41.106.214' // aliyun
+export const API_URL = 'http://121.41.106.214:8080' // aliyun
 // export const API_URL = 'http://localhost:8080'    // 线下测试路径
 // export const API_URL = ' http://127.0.0.1:4523/mock/381770' //apifox
 export const JPA_API_URL = 'http://localhost:8080/jpa'
