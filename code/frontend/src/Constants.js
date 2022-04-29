c825650de328f5854e1ed2c643a354a00ba304ac
yiyikkk <1071080142@qq.com>
2022-04-29 14:22:48 -0400

    upload images to aliyun

--- code/frontend/src/Constants.js
+++ code/frontend/src/Constants.js
@@ -3,12 +3,12 @@
 // export const JPA_API_URL = 'http://localhost:8080/jpa'
 
 // aliyun
-export const API_URL = 'http://121.41.106.214:8080'
-export const FLASK_API_URL = ' http://121.41.106.214:8081'
+// export const API_URL = 'http://121.41.106.214:8080'
+// export const FLASK_API_URL = ' http://121.41.106.214:8081'
 
 // apifox
 // export const API_URL = 'http://127.0.0.1:4523/mock/381770'
 
 // localhost
-// export const API_URL = 'http://localhost:8080'
-// export const FLASK_API_URL = 'http://localhost:8081'
+export const API_URL = 'http://localhost:8080'
+export const FLASK_API_URL = 'http://localhost:8081'
